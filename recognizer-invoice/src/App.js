edb6f217ab73f7d1e16376ccce3c3444a858b20f
AlbiPerez <albiperez1997@gmail.com>
2020-05-29 12:43:20 -0600

    Agregar página web con React

---
+++ recognizer-invoice/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Barra from './componentes/home.js'
+import Stepper from './componentes/stepper.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Barra></Barra>
+      <Stepper></Stepper>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ recognizer-invoice/src/componentes/stepper.js
@@ -0,0 +1,270 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Check from '@material-ui/icons/Check';
+//import SettingsIcon from '@material-ui/icons/Settings';
+//import GroupAddIcon from '@material-ui/icons/GroupAdd';
+//import VideoLabelIcon from '@material-ui/icons/VideoLabel';
+import StepConnector from '@material-ui/core/StepConnector';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import ImageSearchIcon from '@material-ui/icons/ImageSearch';
+import PublishIcon from '@material-ui/icons/Publish';
+import DescriptionIcon from '@material-ui/icons/Description';
+import CheckCircleTwoToneIcon from '@material-ui/icons/CheckCircleTwoTone';
+
+const useQontoStepIconStyles = makeStyles({
+
+  root: {
+    color: '#eaeaf0',
+    display: 'flex',
+    height: 22,
+    alignItems: 'center',
+  },
+  active: {
+    color: '#784af4',
+  },
+  circle: {
+    width: 8,
+    height: 8,
+    borderRadius: '50%',
+    backgroundColor: 'currentColor',
+  },
+  completed: {
+    color: '#784af4',
+    zIndex: 1,
+    fontSize: 18,
+  },
+});
+
+function QontoStepIcon(props) {
+  const classes = useQontoStepIconStyles();
+  const { active, completed } = props;
+
+  return (
+    <div
+      className={clsx(classes.root, {
+        [classes.active]: active,
+      })}
+    >
+      {completed ? <Check className={classes.completed} /> : <div className={classes.circle} />}
+    </div>
+  );
+}
+
+QontoStepIcon.propTypes = {
+  /**
+   * Whether this step is active.
+   */
+  active: PropTypes.bool,
+  /**
+   * Mark the step as completed. Is passed to child components.
+   */
+  completed: PropTypes.bool,
+};
+
+const ColorlibConnector = withStyles({
+  alternativeLabel: {
+    top: 22,
+  },
+  active: {
+    '& $line': {
+      backgroundImage:
+        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+    },
+  },
+  completed: {
+    '& $line': {
+      backgroundImage:
+        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+    },
+  },
+  line: {
+    height: 3,
+    border: 0,
+    backgroundColor: '#eaeaf0',
+    borderRadius: 1,
+  },
+})(StepConnector);
+
+const useColorlibStepIconStyles = makeStyles({
+  root: {
+    backgroundColor: '#ccc',
+    zIndex: 1,
+    color: '#fff',
+    width: 50,
+    height: 50,
+    display: 'flex',
+    borderRadius: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  active: {
+    backgroundImage:
+      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
+  },
+  completed: {
+    backgroundImage:
+      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+  },
+});
+
+function ColorlibStepIcon(props) {
+  const classes = useColorlibStepIconStyles();
+  const { active, completed } = props;
+
+  const icons = {
+    1: <PublishIcon />,
+    2: <ImageSearchIcon />,
+    3: <DescriptionIcon />,
+  };
+
+  return (
+    <div
+      className={clsx(classes.root, {
+        [classes.active]: active,
+        [classes.completed]: completed,
+      })}
+    >
+      {icons[String(props.icon)]}
+    </div>
+  );
+}
+
+ColorlibStepIcon.propTypes = {
+  /**
+   * Whether this step is active.
+   */
+  active: PropTypes.bool,
+  /**
+   * Mark the step as completed. Is passed to child components.
+   */
+  completed: PropTypes.bool,
+  /**
+   * The label displayed in the step icon.
+   */
+  icon: PropTypes.node,
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  button: {
+    marginRight: theme.spacing(1),
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+  buttonSubir: {
+    margin: 10,
+  }
+}));
+
+function getSteps() {
+  return ['Entrenamiento', 'Análisis', 'Resultados'];
+}
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return 'Seleccione las imágenes para el entrenamiento';
+    case 1:
+      return 'Realice el análisis';
+    case 2:
+      return 'Los resultados obtenidos son los siguientes:';
+    default:
+      return 'Paso desconocido';
+  }
+}
+
+export default function CustomizedSteppers() {
+  const classes = useStyles();
+  //asignar en qué stepper empieza
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Stepper alternativeLabel activeStep={activeStep} connector={<ColorlibConnector />} orientation="horizontal">
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
+          
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <Typography color="primary" variant="h5" className={classes.instructions}>
+              Ha completado todos los pasos
+              <CheckCircleTwoToneIcon fontSize='large'></CheckCircleTwoToneIcon>
+             
+            </Typography>
+            <Button variant="contained" onClick={handleReset} className={classes.button}>
+              Reiniciar el proceso?
+            </Button>
+          </div>
+        ) : (
+          <div>
+             <Typography variant="h5" className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+            <div className={classes.root}>
+             <div>
+              <input
+              color="secondary"
+                accept=".pdf"
+                className={classes.input}
+                id="contained-button-file"
+                multiple
+                type="file"
+              />
+              <label htmlFor="contained-button-file">
+                <Button variant="contained" color="secondary" component="span" className={classes.buttonSubir}>
+                  Elegir archivos
+                </Button>
+              </label>
+             </div>
+    </div>
+
+
+            <div className="mb3">
+              <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
+                Anterior
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleNext}
+                className={classes.button}
+              >
+                {activeStep === steps.length - 1 ? 'Finalizar' : 'Siguiente'}
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
