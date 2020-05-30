35a918bbbd479cb4fa3c2e4fe9af42c5e2b56e05
AlbiPerez <albiperez1997@gmail.com>
2020-05-30 13:57:07 -0600

    Agregar endpoint para obtener los blobs de un contenedor.

--- api-factura/App.js
+++ api-factura/App.js
@@ -1,50 +1,33 @@
 const express = require('express');
+const cors = require('cors');
 const formidable = require('formidable');
-const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
+const { BlobServiceClient, StorageSharedKeyCredential } = require('@azure/storage-blob');
 
 const app = express();
-require("dotenv").config();
+require('dotenv').config();
 
-async function main() {
-  // Cuenta y clave del contenedor
-  const account = process.env.ACCOUNT_NAME || "";
-  const accountKey = process.env.ACCOUNT_KEY || "";
+app.use(cors());
 
-  const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
+app.get('/invoice/blobs', async (req, res, next) => {
+	const blobs = [];
+	// Cuenta y clave del contenedor
+	const account = process.env.ACCOUNT_NAME || '';
+	const accountKey = process.env.ACCOUNT_KEY || '';
 
-  //Listar los contenedores
-  const blobServiceClient = new BlobServiceClient(
-    `https://${account}.blob.core.windows.net`,
-    sharedKeyCredential
-  );
-/*
-  let i = 1;
-  for await (const container of blobServiceClient.listContainers()) {
-    console.log(`Container ${i++}: ${container.name}`);
-  }*/
+	const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
 
-  // Crear un contenedor
-  const containerName = 'formrecocontainer';
-  const containerClient = blobServiceClient.getContainerClient(containerName);
+	//Listar los contenedores
+	const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net`, sharedKeyCredential);
 
-  //Listar los PDF del contenedor
-  i = 1;
-  for await (const blob of containerClient.listBlobsFlat()) {
-    console.log(`Blob ${i++}: ${blob.name}`);
-  }
+	// Crear un contenedor
+	const containerName = 'formrecocontainer';
+	const containerClient = blobServiceClient.getContainerClient(containerName);
 
-  // Crear un blob
-  const content = "hello";
-  const blobName = "newblob" + new Date().getTime();
-  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-  const uploadBlobResponse = await blockBlobClient.upload(content, Buffer.byteLength(content));
-  console.log(`Carga del grupo de imágenes ${blobName} exitosamente`, uploadBlobResponse.requestId);
-
-}
-
-main().catch((err) => {
-    console.error("Mensaje de Error:", err.message);
+	//Listar los PDF del contenedor
+	for await (const blob of containerClient.listBlobsFlat()) {
+		blobs.push({ name: blob.name, createdOn: blob.properties.createdOn });
+	}
+	res.json({ data: blobs });
 });
 
 app.listen(3001, () => console.log('Escuchando en el puerto 3001'));
-
