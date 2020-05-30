bdae3b8a542e2475bb2621b0a6cecea649efc7c6
AlbiPerez <albiperez1997@gmail.com>
2020-05-30 15:25:57 -0600

    Agregar endpoint para subir blobs.

--- api-factura/App.js
+++ api-factura/App.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const formidable = require('formidable');
-const { BlobServiceClient, StorageSharedKeyCredential } = require('@azure/storage-blob');
+const blobService = require('./services/blobservice');
 
 const app = express();
 require('dotenv').config();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
+
+const form = formidable({ uploadDir: 'facturas/' });
 
 app.get('/', (req, res) => {
 	res.send(`
@@ -21,18 +26,10 @@ app.get('/', (req, res) => {
 
 app.get('/invoice/blobs', async (req, res, next) => {
 	const blobs = [];
-	// Cuenta y clave del contenedor
-	const account = process.env.ACCOUNT_NAME || '';
-	const accountKey = process.env.ACCOUNT_KEY || '';
 
-	const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
-
-	//Listar los contenedores
-	const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net`, sharedKeyCredential);
-
-	// Crear un contenedor
+	// Obtener el contenedor
 	const containerName = 'formrecocontainer';
-	const containerClient = blobServiceClient.getContainerClient(containerName);
+	const containerClient = blobService.getContainerClient(containerName);
 
 	//Listar los PDF del contenedor
 	for await (const blob of containerClient.listBlobsFlat()) {
@@ -41,8 +38,28 @@ app.get('/invoice/blobs', async (req, res, next) => {
 	res.json({ data: blobs });
 });
 
+app.post('/invoice/blobs', async (req, res, next) => {
+	// Obtener el contenedor
+	const containerName = 'formrecocontainer';
+	const containerClient = blobService.getContainerClient(containerName);
+
+	form.parse(req, async (err, fields, files) => {
+		if (err) {
+			next(err);
+			return;
+		}
+		const { name: blobName } = fields;
+		const content = 'Hello world!';
+
+		const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+		const uploadBlobResponse = await blockBlobClient.upload(content, content.length);
+		console.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse.requestId);
+		res.json({ requestId: uploadBlobResponse.requestId });
+	});
+});
+
 app.post('/api/upload', (req, res, next) => {
-	const form = formidable({ uploadDir: 'facturas/' });
+	// const form = formidable({ uploadDir: 'facturas/' });
 
 	form.on('file', (filename, file) => {
 		form.emit('data', { name: 'file', key: filename, value: file });
