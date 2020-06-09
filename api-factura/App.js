fa8da3db92824dbde226b30c15f0bffc0795cd7e
AlbiPerez <albiperez1997@gmail.com>
2020-06-09 16:29:10 -0600

    Configurar conexión entre React y la API

--- api-factura/App.js
+++ api-factura/App.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const form = formidable(multer({ uploadDir: 'facturas/', multiples: true }));
+const form = formidable({ uploadDir: 'facturas/', multiples: true });
 /*app.use(
 	cors()(
 		multer({
