122e0646ef06fecdda8c46c522b4a7f0e02e80e2
AlbiPerez <albiperez1997@gmail.com>
2020-06-11 19:41:40 -0600

    Corregir el método utilizado para subir las facturas al contenedor.

--- api-factura/App.js
+++ api-factura/App.js
@@ -11,7 +11,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const upload = multer({ dest: 'facturas/' });
+const storage = multer.diskStorage({
+	destination: function(req, file, cb) {
+		cb(null, 'facturas');
+	},
+	filename: function(req, file, cb) {
+		cb(null, Date.now() + file.originalname);
+	}
+});
+
+const upload = multer({ storage: storage });
 
 app.get('/invoice/blobs', async (req, res, next) => {
 	const blobs = [];
@@ -27,22 +36,13 @@ app.get('/invoice/blobs', async (req, res, next) => {
 	res.json({ data: blobs });
 });
 
-function createInvoiceSender(containerClient, factura) {
-	return new Promise((resolve, reject) => {
-		fs.readFile(factura.ruta, 'utf-8', async (err, data) => {
-			if (err) {
-				reject(err);
-			} else {
-				// subir las facturas al servicio de Azure
-				const blockBlobClient = containerClient.getBlockBlobClient(factura.nombre);
-				const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
-				resolve(uploadBlobResponse);
-			}
-		});
-	});
-}
+app.post('/invoice/blobs', upload.array('facturas', 5), async (req, res, next) => {
+	let respuesta = {
+		message: 'Facturas agregadas al contenedor exitosamente!',
+		data: [], // nombre de las facturas subidas exitosamente
+		status: 201 // por defecto el estado http es 201 (creado)
+	};
 
-app.post('/invoice/blobs', upload.array('facturas', 5), (req, res, next) => {
 	// Obtener el contenedor
 	const containerName = 'formrecocontainer';
 	const containerClient = blobService.getContainerClient(containerName);
@@ -53,33 +53,32 @@ app.post('/invoice/blobs', upload.array('facturas', 5), (req, res, next) => {
 		return { nombre, ruta };
 	});
 
-	const invoicesSender = facturas.map((factura) => createInvoiceSender(containerClient, factura));
+	try {
+		for await (const factura of facturas) {
+			// definir el nombre del blob a crear
+			const blockBlobClient = containerClient.getBlockBlobClient(factura.nombre);
 
-	Promise.all(invoicesSender)
-		.then((result) => {
-			console.log(result);
-			res.status(201).send('Las facturas se han subido correctamente.');
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).send(err.message);
-		});
+			// subir al contenedor la factura indicada por la ruta
+			await blockBlobClient.uploadFile(factura.ruta, {
+				concurrency: 5, // 5 es el numero maximo de archivos a subir de forma simultanea
+				onProgress: (ev) => console.log(ev)
+			});
+
+			// agregar la factura subida al conteo
+			respuesta.data.push(factura.nombre);
+		}
+	} catch (err) {
+		console.log(
+			`uploadFile failed, requestId - ${err.details.requestId}, statusCode - ${err.statusCode}, errorCode - ${err
+				.details.errorCode}`
+		);
+
+		// agregar información de error
+		respuesta.message = `Ha ocurrido un error al subir las facturas! [Código de error: ${err.details.errorCode}]`;
+		respuesta.status = 500;
+	}
+
+	res.status(respuesta.status).json(respuesta);
 });
 
 app.listen(3001, () => console.log('Escuchando en el puerto 3001'));
-
-// 		data = {
-// 			message: 'Facturas subidas correctamente!',
-// 			data: invoices, //Devolvemos una o la lista de rutas de los archivos
-// 			nInvoices: upload.length || 1 //Devolvemos la cantidad de archivos
-// 		};
-// 	} else {
-// 		data = {
-// 			message: 'No hay ninguna factura seleccionada para subir...',
-// 			data: [],
-// 			nInvoices: 0
-// 		};
-// 	}
-
-// 	res.json(data);
-// });
